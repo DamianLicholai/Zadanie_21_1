65e4c92d5c5c0f7b146684b49a4d059ec3bebda9
dlich <dlicholai@gmail.com>
2021-07-17 08:35:08 +0200

    Create

---
+++ src/main/java/com/example/zadanie_21_1/Zadanie211Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_21_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie211Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie_21_1/Zadanie211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_21_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
